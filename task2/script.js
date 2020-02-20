aafc589a87e30b45d0a5aaa097abec980d937cef
Andrii <andryshonochek0604@gmail.com>
2020-02-20 23:00:18 +0200

    Add third task (no validation yet)

---
+++ task2/script.js
@@ -0,0 +1,17 @@
+function checkNesting(env1, env2) {
+  console.log(env1, env2);
+  
+
+}
+
+const envelop1 = {
+  a: 10,
+  b: 10,
+};
+
+const envelop2 = {
+  c: 10,
+  d: 10,
+};
+
+checkNesting(envelop1, envelop2);
